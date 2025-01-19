585baf8d87618173ade41f10532446a4378ed5c6
MaciejWojs <misiek111403@gmail.com>
2025-01-19 14:40:30 +0100

    Record class used in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <limits>
+#include "Record.hpp"
+#define FILENAME "Chart-Export2.csv"
 
 int main() {
+    std::ifstream file(FILENAME);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << FILENAME << std::endl;
+        return 1;
+    }
+
+    // int i = 0;
+    std::string line;
+    // std::istringstream ss(line);
+    file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    Record it;
+    file >> it;
+    // while (file >> it) {
+    std::cout << it << std::endl;
+    // if (i++ == 10) break;
+// }
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
